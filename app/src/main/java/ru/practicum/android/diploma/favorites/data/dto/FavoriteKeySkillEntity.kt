ac6853a6ac7e4df1dd6e1f073cc1eb72bb8d2788
elkabelaya <elkabelaya@github.com>
2026-05-27 09:08:31 +0300

    #44 Favorites: Data layer (Room entity, DAO, repository impl) + Domain + DI

---
+++ app/src/main/java/ru/practicum/android/diploma/favorites/data/dto/FavoriteKeySkillEntity.kt
@@ -0,0 +1,26 @@
+package ru.practicum.android.diploma.favorites.data.dto
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+import ru.practicum.android.diploma.vacancy.domain.models.Phone
+
+@Entity(tableName = "favorite_keyskill",
+    foreignKeys = [
+        ForeignKey(
+            entity = FavoriteVacancyEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["vacancyId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["vacancyId"])]
+)
+class FavoriteKeySkillEntity(
+    val vacancyId: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+)
